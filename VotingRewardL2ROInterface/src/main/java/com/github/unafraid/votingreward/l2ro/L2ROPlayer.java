4cb5999546c58db72a75e35d798175ae2529103f
UnAfraid <unafraid89@gmail.com>
2015-10-10 19:51:24 +0300

    Adding L2ro (fandc) interface implementation by @Nik--

---
+++ VotingRewardL2ROInterface/src/main/java/com/github/unafraid/votingreward/l2ro/L2ROPlayer.java
@@ -0,0 +1,93 @@
+/*
+ * Copyright (C) 2004-2015 Vote Rewarding System
+ * 
+ * This file is part of Vote Rewarding System.
+ * 
+ * Vote Rewarding System is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ * 
+ * Vote Rewarding System is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+ * General Public License for more details.
+ * 
+ * You should have received a copy of the GNU General Public License
+ * along with this program. If not, see <http://www.gnu.org/licenses/>.
+ */
+package com.github.unafraid.votingreward.l2ro;
+
+import com.github.unafraid.votingreward.interfaceprovider.api.IPlayerInstance;
+import com.github.unafraid.votingreward.interfaceprovider.model.RewardItemHolder;
+
+import l2ro.gameserver.model.Player;
+import l2ro.gameserver.utils.ItemFunctions;
+
+/**
+ * @author UnAfraid
+ */
+public class L2ROPlayer implements IPlayerInstance
+{
+	private final Player _player;
+	
+	public L2ROPlayer(Player player)
+	{
+		_player = player;
+	}
+	
+	@Override
+	public int getObjectId()
+	{
+		return _player.getObjectId();
+	}
+	
+	@Override
+	public String getName()
+	{
+		return _player.getName();
+	}
+	
+	@Override
+	public boolean isGM()
+	{
+		return _player.isGM();
+	}
+	
+	@Override
+	public void sendMessage(String message)
+	{
+		_player.sendMessage(message);
+	}
+	
+	@Override
+	public void addItem(RewardItemHolder holder)
+	{
+		ItemFunctions.addItem(_player, holder.getId(), holder.getCount(), true, "Reward");
+	}
+	
+	@Override
+	public String getAccountName()
+	{
+		return _player.getAccountName();
+	}
+	
+	@Override
+	public String getIPAddress()
+	{
+		return _player.getIP();
+	}
+	
+	@Override
+	public String getHWID()
+	{
+		// Implement if lameguard is present
+		return null;
+	}
+	
+	@Override
+	public boolean isHWIDSupported()
+	{
+		return getHWID() != null;
+	}
+}
---
+++ VotingRewardL2ROInterface/src/main/java/com/github/unafraid/votingreward/l2ro/L2ROVoicedHandler.java
@@ -0,0 +1,49 @@
+/*
+ * Copyright (C) 2004-2015 Vote Rewarding System
+ * 
+ * This file is part of Vote Rewarding System.
+ * 
+ * Vote Rewarding System is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ * 
+ * Vote Rewarding System is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+ * General Public License for more details.
+ * 
+ * You should have received a copy of the GNU General Public License
+ * along with this program. If not, see <http://www.gnu.org/licenses/>.
+ */
+package com.github.unafraid.votingreward.l2ro;
+
+import com.github.unafraid.votingreward.interfaceprovider.api.IOnVoicedCommandHandler;
+
+import l2ro.gameserver.handler.voicecommands.IVoicedCommandHandler;
+import l2ro.gameserver.model.Player;
+
+/**
+ * @author UnAfraid
+ */
+public class L2ROVoicedHandler implements IVoicedCommandHandler
+{
+	private final IOnVoicedCommandHandler _handler;
+	
+	public L2ROVoicedHandler(IOnVoicedCommandHandler handler)
+	{
+		_handler = handler;
+	}
+	
+	@Override
+	public boolean useVoicedCommand(String command, Player activeChar, String params)
+	{
+		return _handler.useVoicedCommand(command, new L2ROPlayer(activeChar), params);
+	}
+	
+	@Override
+	public String[] getVoicedCommandList()
+	{
+		return _handler.getVoicedCommandList();
+	}
+}
